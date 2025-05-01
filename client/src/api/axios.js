de9e0994276f0ae78956737a5d910226f93b6bb8
dipankar2002 <dipankarganguly15@gmail.com>
2025-05-02 01:03:21 +0530

    Update axios instance baseURL to use environment variable for development mode

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1" : "/api",
   withCredentials: true, // For cookies
 });
