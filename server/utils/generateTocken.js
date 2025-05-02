4408532ce70a67937f90b4a0059c86e5745389ec
dipankar2002 <dipankarganguly15@gmail.com>
2025-05-02 14:06:59 +0530

    Update secure cookie setting to align with production environment

--- server/utils/generateTocken.js
+++ server/utils/generateTocken.js
@@ -4,7 +4,7 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.TOKEN_SECRET, { expiresIn: '7d' });
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
+    secure: process.env.NODE_ENV === 'production',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
